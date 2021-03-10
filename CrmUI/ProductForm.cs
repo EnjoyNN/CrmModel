1014e4d2f79576cda436362777529a5b099b4d18
EnjoyNN <46nik46@gmail.com>
2021-03-10 11:21:06 +0300

    add forms with creating things

---
+++ CrmUI/ProductForm.cs
@@ -0,0 +1,32 @@
+﻿using CrmBuisnessLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CrmUI
+{
+    public partial class ProductForm : Form
+    {
+        public Product Product { get; set; }
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Product = new Product
+            {
+                Name = textBox1.Text,
+                Price = numericUpDown1.Value,
+                Count = Convert.ToInt32(numericUpDown2.Value)
+            };
+            Close();
+        }
+    }
+}
---
+++ CrmUI/SellerForm.cs
@@ -0,0 +1,30 @@
+﻿using CrmBuisnessLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CrmUI
+{
+    public partial class SellerForm : Form
+    {
+        public Seller Seller { get; set; }
+        public SellerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SellerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Seller = new Seller
+            {
+                Name = textBox1.Text
+            };
+            Close();
+        }
+    }
+}
